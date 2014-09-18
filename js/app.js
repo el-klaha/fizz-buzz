a00717da87617581c7b6b648b47b08508421a063
Jonathan <klaha.77@gmail.com>
2014-09-18 16:15:16 -0430

    Finished FizzBuzz Project

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    /*** FIZZBUZZ LOGIC ***/
+
+    var num = prompt("Insert a number from 1 to 100");
+
+    for(var i = 1; i <= num; i++)
+    {
+        if (i % 3 === 0 && i % 5 === 0)
+        {
+            $('body').append("<div>FizzBuzz</div>").;
+        }
+        else if (i % 3 === 0)
+        {
+            $('body').append("<div>Fizz</div>");
+        }
+        else if (i % 5 === 0)
+        {
+            $('body').append("<div>Buzz</div>");
+        }
+        else
+        {
+            $('body').append("<div>"+i+"</div>");
+        }
+    }
+
+});
